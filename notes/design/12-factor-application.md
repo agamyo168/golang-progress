fbf657c4be8d6c567d92152d4093e967fd103d86
agamyo168 <agamyo168@gmail.com>
2026-01-03 02:03:20 +0200

    feat(nix): use nix for reproducibility

--- notes/design/12-factor-application.md
+++ notes/design/12-factor-application.md
@@ -1,4 +1,12 @@
+## 1. Codebase
+
+One codebase tracked in revision control, many deploys
+
+This means that we must have database migrations!
+
 ## 3. Configurable at run-time.
 
 You shall make your application configurable at run-time regardless of where it is deployed (dev/staging/production)
 12-Factor App methodology: your application should simply expect environment variables to be there, regardless of how they got there.
+
+##
---
+++ social/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils"; #This is a helper library. Nix can run on Mac (Intel/M1) and Linux. This utility helps us write the config once and have it work on all those systems automatically.
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            air
+            go-migrate
+            gopls
+          ];
+
+          shellHook = ''
+            echo "🐹 Gopher Environment Active"
+            echo "Go version: $(go version)"
+          '';
+        };
+      });
+}
